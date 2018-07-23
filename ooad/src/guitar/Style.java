26b806b0bbff363e4848aa6ce169fc9defec61df
dmeka <dmeka@daniellmekasmbp>
2018-07-23 15:09:58 +1000

    Added style enum for Mandolin

---
+++ ooad/src/guitar/Style.java
@@ -0,0 +1,16 @@
+package guitar;
+
+public enum Style {
+
+	
+	A, F;
+	
+	public String toString() { 
+		switch(this) {
+			case A: return "a";
+			case F: return "f";
+			default: return " ";
+		}
+	}
+
+}
